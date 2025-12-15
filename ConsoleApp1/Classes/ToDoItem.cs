e495eaf096651967d80914964768db7ff8251421
Dmitry <vatuev.dima@yandex.ru>
2025-12-15 20:08:27 +0400

    Добавлены 3 класса для юзера, задачи и перечисление. Добавлены 2 метода для задачи, метод для получения username и указание полей количество и длина задачи перемещены в метод /start. Так же добавлено ограничение использования комманд без использованной комманды /start

---
+++ ConsoleApp1/Classes/ToDoItem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Classes
+{
+    internal class ToDoItem
+    {
+        public Guid id { get; }
+        public ToDoUser User { get;}
+        public string Name { get; }
+        public DateTime CreatedAt { get; }
+        private ToDoItemState _state;
+        public ToDoItemState State { 
+            get 
+            { 
+                return _state;
+            }
+            set 
+            {
+                _state = value;
+                StateChangedAt = DateTime.UtcNow;
+            }
+        }
+        public DateTime? StateChangedAt { get; private set; }
+        public ToDoItem(ToDoUser user, string name)
+        {
+            id = Guid.NewGuid();
+            User = user;
+            Name = name;
+            CreatedAt = DateTime.UtcNow;
+            State = ToDoItemState.Actrive;
+        }
+    }
+}
